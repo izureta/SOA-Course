4612e5b4749fc4d0493fefa7a7404f31883f2e01
izureta <kruchininila@gmail.com>
2024-03-15 01:38:13 +0300

    Fix

--- source/app.py
+++ source/app.py
@@ -1,26 +1,17 @@
-from flask import Flask, request
+from flask import request, session
 from flask_sqlalchemy import SQLAlchemy
 from werkzeug.security import generate_password_hash
 
-app = Flask('Social network')
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://iliakruchinin@localhost/mydatabase'
-db = SQLAlchemy(app)
+from . import create_app
 
-class User(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(80), unique=True, nullable=False)
-    password_hash = db.Column(db.String(120), nullable=False)
-    name = db.Column(db.String(100), nullable=True)
-    surname = db.Column(db.String(100), nullable=True)
-    birth_date = db.Column(db.Date, nullable=True)
-    email = db.Column(db.String(100), nullable=True)
-    phone = db.Column(db.String(20), nullable=True)
+from .models import User, db
 
+app = create_app()
 
 @app.route('/register', methods=['POST'])
 def register():
     data = request.get_json()
-    hashed_password = generate_password_hash(data['password'], method='sha256')
+    hashed_password = generate_password_hash(data['password'])
     new_user = User(username=data['username'], password_hash=hashed_password)
     db.session.add(new_user)
     db.session.commit()
@@ -30,12 +21,17 @@ def register():
 def update_user():
     data = request.get_json()
     user = User.query.filter_by(username=data['username']).first()
-    if user and check_password_hash(user.password, data['password']):
-        user.name = data['name']
-        user.surname = data['surname']
-        user.birth_date = data['birth_date']
-        user.email = data['email']
-        user.phone = data['phone']
+    if user and check_password_hash(user.password_hash, data['password']):
+        if 'name' in data:
+            user.name = data['name']
+        if 'surname' in data:
+            user.surname = data['surname']
+        if 'birth_date' in data:
+            user.birth_date = data['birth_date']
+        if 'email' in data:
+            user.email = data['email']
+        if 'phone' in data:
+            user.phone = data['phone']
         db.session.commit()
         return {'message': 'User dsata updated'}
     else:
@@ -47,7 +43,8 @@ from werkzeug.security import check_password_hash
 def login():
     data = request.get_json()
     user = User.query.filter_by(username=data['username']).first()
-    if user and check_password_hash(user.password, data['password']):
+    if user and check_password_hash(user.password_hash, data['password']):
+        session[user.username] = user.id
         return {'message': 'Logged in'}
     return {'message': 'Wrong username or password'}
 
